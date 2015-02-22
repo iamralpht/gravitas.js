5f71a47e4ee442eca989550bcb4c85876c5b49ea
Ralph Thomas <ralpht@gmail.com>
2015-02-22 12:54:29 -0800

    Add 1D scrolling example from blog post.

---
+++ examples/Scrolling/scrolling.js
@@ -0,0 +1,134 @@
+'use strict';
+/*
+Copyright 2014 Ralph Thomas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/***
+ * Scroll combines Friction and Spring to provide the
+ * classic "flick-with-bounce" behavior.
+ */
+function Scroll(extent) {
+    this._extent = extent;
+    this._friction = new Gravitas.Friction(0.01);
+    this._spring = new Gravitas.Spring(1, 90, 20);
+    this._startTime = 0;
+    this._springing = false;
+    this._springOffset = 0;
+}
+Scroll.prototype.set = function(x, v) {
+    this._friction.set(x, v);
+
+    // If we're over the extent or zero then start springing. Notice that we also consult
+    // velocity because we don't want flicks that start in the overscroll to get consumed
+    // by the spring.
+    if (x > 0 && v >= 0) {
+        this._springOffset = 0;
+        this._springing = true;
+        this._spring.snap(x);
+        this._spring.setEnd(0);
+    } else if (x < -this._extent && v <= 0) {
+        this._springOffset = 0;
+        this._springing = true;
+        this._spring.snap(x);
+        this._spring.setEnd(-this._extent);
+    } else {
+        this._springing = false;
+    }
+    this._startTime = (new Date()).getTime();
+}
+Scroll.prototype.x = function(t) {
+    if (!this._startTime) return 0;
+    if (!t) t = ((new Date()).getTime() - this._startTime) / 1000.0;
+    // We've entered the spring, use the value from there.
+    if (this._springing) return this._spring.x() + this._springOffset;
+    // We're still in friction.
+    var x = this._friction.x(t);
+    var dx = this.dx(t);
+    // If we've gone over the edge the roll the momentum into the spring.
+    if ((x > 0 && dx >= 0) || (x < -this._extent && dx <= 0)) {
+        this._springing = true;
+        this._spring.setEnd(0, dx);
+        if (x < -this._extent) this._springOffset = -this._extent;
+        else this._springOffset = 0;
+        x = this._spring.x() + this._springOffset;
+    }
+    return x;
+}
+Scroll.prototype.dx = function(t) {
+    if (this._springing) return this._spring.dx(t);
+    return this._friction.dx(t);
+}
+Scroll.prototype.done = function() {
+    if (this._springing) return this._spring.done();
+    else return this._friction.done();
+}
+Scroll.prototype.configuration = function() {
+    var config = this._friction.configuration();
+    config.push.apply(config, this._spring.configuration());
+    return config;
+}
+
+var UNDERSCROLL_TRACKING = 0.5;
+
+function ScrollHandler(element) {
+    this._element = element;
+    this._position = 0;
+    this._extent = this._element.offsetHeight - this._element.parentElement.offsetHeight;
+    this._scroll = new Scroll(this._extent);
+}
+ScrollHandler.prototype.onTouchStart = function() {
+    this._startPosition = this._position;
+
+    // Ensure that we don't jump discontinuously when applying the underscroll
+    // tracking in onTouchMove if the view is currently outside of the valid
+    // scroll constraints.
+    if (this._startPosition > 0)
+        this._startPosition /= UNDERSCROLL_TRACKING;
+    else if (this._startPosition < -this._extent)
+        this._startPosition = (this._startPosition + this._extent) / UNDERSCROLL_TRACKING - this._extent;
+
+    if (this._animation) this._animation.cancel();
+
+    var pos = this._position;
+    var transform = 'translateY(' + pos + 'px)';
+    this._element.style.webkitTransform = transform;
+    this._element.style.transform = transform;
+}
+ScrollHandler.prototype.onTouchMove = function(dx, dy) {
+    var pos = dy + this._startPosition;
+    if (pos > 0) pos *= UNDERSCROLL_TRACKING;
+    else if (pos < -this._extent) pos = (pos + this._extent) * UNDERSCROLL_TRACKING - this._extent;
+
+    this._position = pos;
+    var transform = 'translateY(' + pos + 'px) translateZ(0)';
+    this._element.style.webkitTransform = transform;
+    this._element.style.transform = transform;
+}
+ScrollHandler.prototype.onTouchEnd = function(dx, dy, velocity) {
+    var self = this;
+    this._scroll.set(this._position, velocity.y);
+    this._animation = Gravitas.createAnimation(this._scroll, function() {
+        var pos = self._scroll.x();
+        self._position = pos;
+        // The translateZ is to help older WebKits not collapse this layer into a non-composited layer
+        // since they're also slow at repaints.
+        var transform = 'translateY(' + pos + 'px) translateZ(0)';
+        self._element.style.webkitTransform = transform;
+        self._element.style.transform = transform;
+    });
+}
+ScrollHandler.prototype.configuration = function() {
+    return this._scroll.configuration();
+}
---
+++ examples/Scrolling/scrollingexample.js
@@ -0,0 +1,72 @@
+'use strict';
+/*
+Copyright 2014 Ralph Thomas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+ * Scrolling example. Simple list-of-contacts thing from 2007.
+ */
+function ScrollingDemo(element) {
+    this._element = element;
+    this._element.classList.add('scrollingdemo');
+
+    // The outer is the frame that doesn't move. This contains the long block that moves
+    // up and down (clipped to the outer).
+    this._outer = document.createElement('div');
+    this._outer.className = 'scrolling-outer';
+
+    this._scroller = document.createElement('div');
+    this._scroller.className = 'scrolling-inner';
+
+    this._outer.appendChild(this._scroller);
+
+    var model = [
+        { url: 'albums/radiohead.jpg', band: 'Radiohead' },
+        { url: 'albums/abbey-road.jpg', band: 'The Beatles' },
+        { url: 'albums/1984.jpg', band: 'Van Halen' },
+        { url: 'albums/thom-yorke.jpg', band: 'Thom Yorke' },
+        { url: 'albums/agaetis-byrjun.jpg', band: 'Sigur Ros' },
+        { url: 'albums/unknown-pleasures.jpg', band: 'Joy Division' },
+        { url: 'albums/hot-chip.jpg', band: 'Hot Chip' },
+        { url: 'albums/cross.jpg', band: 'Justice' },
+        { url: 'albums/massive-attack.jpg', band: 'Massive Attack' },
+        { url: 'albums/the-xx.jpg', band: 'The xx' }
+    ];
+    // Make some list items
+    for (var i = 0; i < 30; i++) {
+        var m = model[i % model.length];
+        var d = document.createElement('div');
+        d.className = 'scrolling-list-item';
+        var img = document.createElement('div');
+        img.className = 'scrolling-list-img';
+        img.style.backgroundImage = 'url(../iTunesRadio/' + m.url + ')';
+        d.appendChild(img);
+        var text = document.createElement('span');
+        text.textContent = m.band;
+        d.appendChild(text);
+        this._scroller.appendChild(d);
+    }
+
+    this._element.appendChild(this._outer);
+    
+    var scrollHandler = new ScrollHandler(this._scroller);
+    addTouchOrMouseListener(this._outer, scrollHandler);
+
+    var controls = new Controls();
+    controls.addModel(scrollHandler.configuration(), 'Scrolling');
+    this._element.appendChild(controls.element());
+}
+
+window.addEventListener('load', function() { new ScrollingDemo(document.getElementById('scrollingExample')); }, false);
